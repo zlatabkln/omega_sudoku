8fdcbdc0ea4293409c9d1d71751987c45c9888f3
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2023-01-15 23:17:52 +0200

    Add files via upload

---
+++ Factory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    class Factory
+    {
+        public Constraint createConstraint(string cType, int par1, int par2)
+        {
+            return new Constraint(cType, par1, par2);
+        }
+        public Variant createVariant(int row, int col, int val)
+        {
+            return new Variant(row, col, val);
+        }
+    }
+}
