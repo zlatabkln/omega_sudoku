04e8f07e10c643dd862ae16ce1dd8902375cadbe
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2023-01-15 23:10:23 +0200

    Update Variant.cs

--- Variant.cs
+++ Variant.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +13,21 @@ namespace Sudoku
         private int val;
         public Variant(int row, int col, int val)
         {
-
+            this.row = row;
+            this.col = col;
+            this.val = val;
+        }
+        public int getRow()
+        {
+            return this.row;
+        }
+        public int getCol()
+        {
+            return this.col;
+        }
+        public int getValue()
+        {
+            return this.val;
         }
     }
 }
