998617c6e26fcf12c79dcd4669d80a87098b39d6
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2023-01-16 20:55:17 +0200

    Update Factory.cs

--- Factory.cs
+++ Factory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace Sudoku
 {
+    //factory is used to create objects of two types - constraints and variants
     class Factory
     {
+        //function to create constraints with given fields
         public Constraint createConstraint(string cType, int par1, int par2)
         {
             return new Constraint(cType, par1, par2);
         }
+        //function to create variants with given fields
         public Variant createVariant(int row, int col, int val)
         {
             return new Variant(row, col, val);
