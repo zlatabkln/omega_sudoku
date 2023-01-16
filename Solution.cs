d9ff4b16cefd57ce2a898f7a9a0fba052dc31cea
zlatabkln <60300554+zlatabkln@users.noreply.github.com>
2023-01-16 20:55:40 +0200

    Update Solution.cs

--- Solution.cs
+++ Solution.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,44 +6,66 @@ using System.Threading.Tasks;
 
 namespace Sudoku
 {
+    //this class creates a solution from board
     partial class Solution
     {
         private int[,] solution;
         public Solution(int[,] grid, int size)
         {
+            //factory - to fill the dictionaries
             Factory f = new Factory();
+            //square root from number of rows
             int R = size;
+            //square root from number of columns
             int C = size;
+            //square root from number of cells
             int N = R * C;
+            //list of all the constraints in board
             List<Constraint> listOfConstraints = new List<Constraint>();
-            addConstraints(listOfConstraints, N, "rc",f);
-            addConstraints(listOfConstraints, N, "rn",f);
-            addConstraints(listOfConstraints, N, "cn",f);
-            addConstraints(listOfConstraints, N, "bn",f);
+            //fill the list
+            addConstraints(listOfConstraints, N, "rc", f);
+            addConstraints(listOfConstraints, N, "rn", f);
+            addConstraints(listOfConstraints, N, "cn", f);
+            addConstraints(listOfConstraints, N, "bn", f);
+            //dictionary of variants and lists of constraints each variant covers
             Dictionary<Variant, List<Constraint>> varDict = new Dictionary<Variant, List<Constraint>>();
-            addVariants(varDict,listOfConstraints, N, R, C, f);
-            Dictionary<Constraint, HashSet<Variant>> xDict = exact_cover(listOfConstraints, varDict);
+            //fill the dictionary
+            addVariants(varDict, listOfConstraints, N, R, C, f);
+            //create a dictionary of constraints and lists of values that cover them
+            Dictionary<Constraint, List<Variant>> xDict = exact_cover(listOfConstraints, varDict);
+            //delete all the existing values and match the constraints
+            //stay with dictionary of uncovered constraints and lists of possible variants to cover them
             for (int i = 0; i < grid.GetLength(0); i++)
             {
                 for (int j = 0; j < grid.GetLength(1); j++)
                 {
-                    if (grid[i,j] != 0)
+                    if (grid[i, j] != 0)
                     {
-                        select(xDict, varDict, new Variant(i, j, grid[i,j]));
+                        select(xDict, varDict, new Variant(i, j, grid[i, j]));
                     }
                 }
             }
+            //list for solution
             List<Variant> solutions = new List<Variant>();
-            foreach (var solution in solve(xDict, varDict, solutions))
+            try
             {
-                List<Variant> sol = solve(xDict, varDict, solutions).ToList();
-                Variant v;
-                for (int i = 0; i < sol.Count(); i++)
+                //solve the board
+                solutions = solve(xDict, varDict, solutions);
+                //solution not found
+                if (solutions.Count == 0)
+                    throw new SudokuError("Impossible to solve");
+                //fill the board with solution
+                foreach (Variant solution in solve(xDict, varDict, solutions))
                 {
-                    v=sol[i];
-                    grid[v.getRow(),v.getCol()] = v.getValue();
+                    grid[solution.getRow(), solution.getCol()] = solution.getValue();
                 }
                 this.solution = grid;
+            }
+            //impossible board exception
+            catch (SudokuError se)
+            {
+                Console.WriteLine(string.Format("{0}:{1}", se.GetType(), se.Message));
+                throw;
             }
         }
         public int[,] getSolution()
